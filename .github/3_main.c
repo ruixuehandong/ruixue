4b2822953efeeb883549b2e200e287a2c7ee3761
ruixuehandong <3272281276@qq.com>
2025-10-25 13:27:32 +0800

    Create 3_main.c

---
+++ .github/3_main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ int main() {
+     int a, b;
+     char op;
+     scanf("%d %d %c", &a, &b, &op);
+     switch (op) {
+         case '+':
+             printf("%d\n", a + b);
+             break;
+         case '-':
+             printf("%d\n", a - b);
+             break;
+         case '*':
+             printf("%d\n", a * b);
+             break;
+         case '/':
+             if (b != 0) {
+                 printf("%d\n", a / b);
+             }
+             break;
+         default:
+             break;
+     }
+     return 0;
+ }
