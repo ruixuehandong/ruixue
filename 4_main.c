8b4016b9cb1575f25e268136e37ba14f187c8c19
ruixuehandong <3272281276@qq.com>
2025-10-25 13:54:38 +0800

    Create 4_main.c

---
+++ 4_main.c
@@ -0,0 +1,14 @@
+//202510306225
+//3272281276@qq.com
+//邓鑫瑞
+#include <stdio.h>
+ int main() {
+     int a, b, c;
+     scanf("%d %d %d", &a, &b, &c);
+     if (a + b > c && a + c > b && b + c > a) {
+         printf("可以组成三角形\n");
+     } else {
+         printf("不能组成三角形\n");
+     }
+     return 0;
+ }
