d98331f34a0e1bcd802dfa0dde3d8ca422888311
ruixuehandong <3272281276@qq.com>
2025-10-25 13:48:11 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,20 +1,11 @@
 //202510306225
 //3272281276@qq.com
 //邓鑫瑞
-#include <stdio.h>
- int main() {
-     int score;
-     scanf("%d", &score);
-     if (score >= 90 && score <= 100) {
-         printf("A\n");
-     } else if (score >= 80 && score < 90) {
-         printf("B\n");
-     } else if (score >= 70 && score < 80) {
-         printf("C\n");
-     } else if (score >= 60 && score < 70) {
-         printf("D\n");
-     } else if (score >= 0 && score < 60) {
-         printf("E\n");
-     }
-     return 0;
- }
+#include<stdio.h>
+int main(){
+    float celsius;
+    scanf("%f",&celsius);
+    float fahrenheit = celsius * 9/5 + 32;
+    printf("%.1f\n",fahrenheit);
+    return 0;
+}
