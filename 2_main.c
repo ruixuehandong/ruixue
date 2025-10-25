eaa67b48a204ccb591e3b6b3cde6d98fefc07dff
ruixuehandong <3272281276@qq.com>
2025-10-25 13:51:36 +0800

    Create 2_main.c

---
+++ 2_main.c
@@ -0,0 +1,20 @@
+//202510306225
+//3272281276@qq.com
+//邓鑫瑞
+#include <stdio.h>
+ int main() {
+     int score;
+     scanf("%d", &score);
+     if (score >= 90 && score <= 100) {
+         printf("A\n");
+     } else if (score >= 80 && score < 90) {
+         printf("B\n");
+     } else if (score >= 70 && score < 80) {
+         printf("C\n");
+     } else if (score >= 60 && score < 70) {
+         printf("D\n");
+     } else if (score >= 0 && score < 60) {
+         printf("E\n");
+     }
+     return 0;
+ }
