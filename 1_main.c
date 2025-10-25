9dffbe7a6a0b9ed3d9ef457da34831963a657168
ruixuehandong <3272281276@qq.com>
2025-10-25 13:22:37 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,8 @@
-//
-//
-//
-#include <stdio.h>
-int main()
-{
-    printf("Hello World");
+#include<stdio.h>
+int main(){
+    float celsius;
+    scanf("%f",&celsius);
+    float fahrenheit = celsius * 9/5 + 32;
+    printf("%.1f\n",fahrenheit);
     return 0;
 }
