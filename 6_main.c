92c5502efbf1dab8ff8bc50a0bcec1b6e7257e38
ruixuehandong <3272281276@qq.com>
2025-11-01 09:11:11 +0800

    Update 6_main.c

--- 6_main.c
+++ 6_main.c
@@ -3,18 +3,18 @@
 //邓鑫瑞
 #include <stdio.h>
 int main() {
-    int i, bai, shi, ge;
-    // 遍历所有三位数
-    for (i = 100; i <= 999; i++) {
-        bai = i / 100;    // 提取百位
-        shi = i / 10 % 10;// 提取十位
-        ge = i % 10;      // 提取个位
-        // 判断是否满足水仙花数条件
-        if (bai * bai * bai + shi * shi * shi + ge * ge * ge == i) {
-            printf("%d ", i);
-        }
+    int arr[5], i, num;
+    for (i = 0; i < 5; i++) {
+        // 循环输入，直到获取偶数
+        do {
+            scanf("%d", &num);
+        } while (num % 2 != 0);
+        arr[i] = num;
     }
-    // 移除最后一个空格（优化输出格式）
+    // 输出全偶数组
+    for (i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
     printf("\b");
     return 0;
 }
