240c03cd1b3db4cb407e26d8bb041aa18cb35620
ruixuehandong <3272281276@qq.com>
2025-11-01 09:10:52 +0800

    Update 5_main.c

--- 5_main.c
+++ 5_main.c
@@ -3,20 +3,17 @@
 //邓鑫瑞
 #include <stdio.h>
 int main() {
-    int n, i, is_prime = 1;
-    scanf("%d", &n);
-    // 1不是质数，2是最小质数，大于2的偶数不是质数
-    if (n == 1) is_prime = 0;
-    else if (n == 2) is_prime = 1;
-    else if (n % 2 == 0) is_prime = 0;
-    // 遍历3到n-1的奇数判断是否能整除n
-    for (i = 3; i * i <= n; i += 2) {
-        if (n % i == 0) {
-            is_prime = 0;
-            break;
-        }
+    int arr[5], i, sum = 0;
+    // 输入前四位并计算和
+    for (i = 0; i < 4; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
     }
-    if (is_prime) printf("密钥安全，密码设置成功\n");
-    else printf("密钥不安全，请重新输入\n");
+    arr[4] = sum;
+    // 输出完整数组
+    for (i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\b");
     return 0;
 }
