0fbd046d613db1a1b0c1d4dc900a7c639138ff6f
ruixuehandong <3272281276@qq.com>
2025-11-08 09:50:38 +0800

    Implement 3x3 matrix input and output
    Replaced prime number check with 3x3 matrix input and output.

--- 1_main.c
+++ 1_main.c
@@ -2,21 +2,21 @@
 //3272281276@qq.com
 //邓鑫瑞
 #include <stdio.h>
+
 int main() {
-    int n, i, is_prime = 1;
-    scanf("%d", &n);
-    // 1不是质数，2是最小质数，大于2的偶数不是质数
-    if (n == 1) is_prime = 0;
-    else if (n == 2) is_prime = 1;
-    else if (n % 2 == 0) is_prime = 0;
-    // 遍历3到n-1的奇数判断是否能整除n
-    for (i = 3; i * i <= n; i += 2) {
-        if (n % i == 0) {
-            is_prime = 0;
-            break;
+    int arr[3][3];
+    // 输入3*3矩阵
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &arr[i][j]);
         }
     }
-    if (is_prime) printf("密钥安全，密码设置成功\n");
-    else printf("密钥不安全，请重新输入\n");
+    // 原样输出
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
