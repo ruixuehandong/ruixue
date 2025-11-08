8ee175f88fd11a012e1807638b740981341d7d4e
ruixuehandong <3272281276@qq.com>
2025-11-08 09:52:40 +0800

    Refactor main function and add sum/product functions

--- 6_main.c
+++ 6_main.c
@@ -2,19 +2,35 @@
 //3272281276@qq.com
 //邓鑫瑞
 #include <stdio.h>
+
+// 计算数组所有元素的和
+int arraySum(int arr[], int len) {
+    int sum = 0;
+    for (int i = 0; i < len; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+// 计算数组所有元素的积
+int arrayProduct(int arr[], int len) {
+    int product = 1;
+    for (int i = 0; i < len; i++) {
+        product *= arr[i];
+    }
+    return product;
+}
+
 int main() {
-    int arr[5], i, num;
-    for (i = 0; i < 5; i++) {
-        // 循环输入，直到获取偶数
-        do {
-            scanf("%d", &num);
-        } while (num % 2 != 0);
-        arr[i] = num;
+    int arr[5];
+    int len = 5;
+    // 输入长度为5的数组
+    for (int i = 0; i < len; i++) {
+        scanf("%d", &arr[i]);
     }
-    // 输出全偶数组
-    for (i = 0; i < 5; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\b");
+    // 调用函数并输出结果
+    int sum = arraySum(arr, len);
+    int product = arrayProduct(arr, len);
+    printf("%d %d\n", sum, product);
     return 0;
 }
