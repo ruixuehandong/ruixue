29fc15e84d704547a3a7944626cb51620a01778d
ruixuehandong <3272281276@qq.com>
2025-11-08 09:53:03 +0800

    Update 7_main.c

--- 7_main.c
+++ 7_main.c
@@ -2,21 +2,34 @@
 //3272281276@qq.com
 //邓鑫瑞
 #include <stdio.h>
+
+// 反转数组元素（输入数组首地址和长度）
+void reverseArray(int arr[], int len) {
+    for (int i = 0; i < len / 2; i++) {
+        // 首尾元素交换
+        int temp = arr[i];
+        arr[i] = arr[len - 1 - i];
+        arr[len - 1 - i] = temp;
+    }
+}
+
 int main() {
-    int arr[5] = {0}, i;
-    // 输入前四位（存储在arr[0]-arr[3]）
-    for (i = 0; i < 4; i++) {
+    int arr[5];
+    int len = 5;
+    // 输入长度为5的数组
+    for (int i = 0; i < len; i++) {
         scanf("%d", &arr[i]);
     }
-    // 元素整体后移一位（arr[0]→arr[1]，…，arr[3]→arr[4]）
-    for (i = 4; i > 0; i--) {
-        arr[i] = arr[i - 1];
+    // 调用反转函数
+    reverseArray(arr, len);
+    // 输出反转结果（1后无空格）
+    for (int i = 0; i < len; i++) {
+        if (i == len - 1) {
+            printf("%d", arr[i]);
+        } else {
+            printf("%d ", arr[i]);
+        }
     }
-    arr[0] = 0; // 首位补0
-    // 输出更新后数组
-    for (i = 0; i < 5; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\b");
+    printf("\n");
     return 0;
 }
